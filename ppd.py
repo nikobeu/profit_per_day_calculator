83a02514a03499bd6a6dc2be09fb555ed8824848
nikobeu <nikolai.luedtke@web.de>
2023-10-09 13:45:13 +0200

    schneller neustart und schneller exit

--- ppd.py
+++ ppd.py
@@ -38,19 +38,33 @@ else:
         os.system(nstart)
         exit()
     else:
-        exiting = False
         liste = False
         print("wenn du stoppen willst drücke zu dem zeitpunkt x und wenn du irgendwann neustarten willst drücke n")
         sauswahl = input("willst du deín ergebnis speichern ? JA/Nein :").lower()
-
-        if sauswahl == "ja":
+        if sauswahl == "x":
+            print("abgebrochen")
+            input("zum schließen irgendeine taste drücken")
+            exit()
+        elif sauswahl == "n":
+            print("neustart")
+            os.system(nstart)
+            exit()
+        elif sauswahl == "ja":
             liste = True
         else:
             liste = False
         auswahl = input(
             "drücke z zum ziel berechnen oder b zum berechnen vom endergebnis oder t für eine anzahl an tagen : ").lower()
 
-        if auswahl == "b":
+        if sauswahl == "x":
+            print("abgebrochen")
+            input("zum schließen irgendeine taste drücken")
+            exit()
+        elif sauswahl == "n":
+            print("neustart")
+            os.system(nstart)
+            exit()
+        elif auswahl == "b":
 
             print("du bist im berechnen bereich")
 
@@ -60,135 +74,51 @@ else:
 
             ppd = input("prozent pro tag eingeben : ")
 
-            tagerechn1 = float(tage) + 1
-
-            listename = f"von {str(start)} mit {str(ppd)} % für {str(tage)} tage liste.txt"
-
-            if liste == True:
-                if os.path.exists(listename):
-                    print("genau diese datei gibt es bereits")
-                    liste = False
-                else:
-                    file = open(str(listename), "a", encoding='utf-8')
-                    file.write(f"von {str(start)} mit {str(ppd)} % für {str(tage)} tage : \n")
-                    file.close()
-            else:
-                print("du hast dich gegen die liste entschieden")
-
-            tagedone = 0
-            startr = 0
-
-            profitber = (float(ppd) / 100) + 1
-            while float(tagedone) < float(tagerechn1):
-                startr = round(float(start), 2)
-                if liste == True:
-                    file = open(str(listename), "a")
-                    file.write(f"tag {str(tagedone)} : {str(startr)} \n")
-                    file.close()
-                start = float(start) * float(profitber)
-                tagedone += 1
-
-            print(str(startr), "bei", str(ppd), "%")
-
-            neustart = input("n für neustart, irgendeine andere taste zum verlassen : ").lower()
-            if neustart == "n":
+            if start == "x":
+                print("abgebrochen")
+                input("zum schließen irgendeine taste drücken")
+                exit()
+            elif start == "n":
+                print("neustart")
                 os.system(nstart)
                 exit()
-            else:
+            elif tage == "n":
+                print("neustart")
+                os.system(nstart)
+                exit()
+            elif tage == "x":
+                print("abgebrochen")
+                input("zum schließen irgendeine taste drücken")
+                exit()
+            elif ppd == "n":
+                print("neustart")
+                os.system(nstart)
+                exit()
+            elif ppd == "x":
+                print("abgebrochen")
                 input("zum schließen irgendeine taste drücken")
                 exit()
 
-
-        elif auswahl == "z":
-            print("du bist im zeit such bereich")
-            start = input("start angeben : ")
-            ziel = input("Ziel eingeben : ")
-            ppd = input("wass ist ein realistischer profit in % pro tag : ")
-            tagen = 1
-            startr = 0
-            listename = f"von {str(start)} mit {str(ppd)} % bis {str(ziel)} liste.txt"
-
-            if liste == True:
-                if os.path.exists(listename):
-                    print("genau diese datei gibt es bereits")
-                    liste = False
-                else:
-                    file = open(str(listename), "a")
-                    file.write(f"von {str(start)} mit {str(ppd)} % bis {str(ziel)} : \n")
-                    file.write(f"tag : 0 : {str(start)} \n")
-                    file.close()
             else:
-                print("du hast dich gegen die liste entschieden")
+                tagerechn1 = float(tage) + 1
 
-            if start == "0":
-                print("der startpunkt ist zu klein!")
-            else:
-                rechn1 = (float(ppd) / 100) + 1
+                listename = f"von {str(start)} mit {str(ppd)} % für {str(tage)} tage liste.txt"
 
-                while float(start) < float(ziel):
-                    startr = round(float(start), 2)
-                    start = float(start) * float(rechn1)
-                    if liste == True:
-                        file = open(str(listename), "a")
-                        file.write(f"tag : {str(tagen)} : {str(startr)} \n")
+                if liste == True:
+                    if os.path.exists(listename):
+                        print("genau diese datei gibt es bereits")
+                        liste = False
+                    else:
+                        file = open(str(listename), "a", encoding='utf-8')
+                        file.write(f"von {str(start)} mit {str(ppd)} % für {str(tage)} tage : \n")
                         file.close()
-                    tagen += 1
-                if float(start) > float(ziel):
-                    print("")
-
-                    if liste == True:
-                        file = open(str(listename), "a")
-                        file.write(f"tag : {str(tagen)} : {str(ziel)} \n")
-                        file.close()
-
-                print("es braucht ", str(tagen), "tag(e) bis du ", str(ziel), "erreicht hast")
-
-                neustart = input("n für neustart, irgendeine andere taste zum verlassen : ").lower()
-                if neustart == "n":
-                    os.system(nstart)
-                    exit()
                 else:
-                    input("zum schließen irgendeine taste drücken")
-                    exit()
-
-        elif auswahl == "t":
-            print("du bist im tage bereich")
-            start = input("startpunkt eingeben : ")
-            ziel = input("gib dein ziel ein : ")
-            tage = input("wie viele tage hast du zeit : ")
-            listename = f"von {str(start)} zu {str(ziel)} in {str(tage)} tagen liste.txt"
-            tagerechn1 = float(tage) + 1
-            mindtage = 1
-
-            if float(tage) < float(mindtage):
-                print("du hast zu wenig tage eingegeben bitte versuche es erneut")
-                tage = input("tage wiederholen : ")
-            else:
-                print("")
-
-            A = float(start)
-            E = float(ziel)
-            T = float(tage)
-            # rechnung, do not touch !!!
-            P = ((E / A) ** (1 / T)) - 1
-
-            proz = float(P) * 100
-            prozr = round(proz, 2)
-
-            if liste == True:
-                if os.path.exists(listename):
-                    print("genau diese datei gibt es bereits")
-                    liste = False
-                else:
-                    file = open(str(listename), "a")
-                    file.write(f"von {str(start)} zu {str(ziel)} in {str(tage)} benötigt : {str(prozr)} % \n")
-                    file.write(f"das Kapital würde so aussehen : \n")
-                    file.close()
+                    print("du hast dich gegen die liste entschieden")
 
                 tagedone = 0
                 startr = 0
 
-                profitber = (float(proz) / 100) + 1
+                profitber = (float(ppd) / 100) + 1
                 while float(tagedone) < float(tagerechn1):
                     startr = round(float(start), 2)
                     if liste == True:
@@ -198,15 +128,177 @@ else:
                     start = float(start) * float(profitber)
                     tagedone += 1
 
-            print("es braucht ca.", str(prozr), "% pro tag")
+                print(str(startr), "bei", str(ppd), "%")
 
-            neustart = input("n für neustart, irgendeine andere taste zum verlassen : ").lower()
-            if neustart == "n":
-                os.system(nstart)
-                exit()
-            else:
+                neustart = input("n für neustart, irgendeine andere taste zum verlassen : ").lower()
+                if neustart == "n":
+                    os.system(nstart)
+                    exit()
+                else:
+                    input("zum schließen irgendeine taste drücken")
+                    exit()
+
+        elif auswahl == "z":
+            print("du bist im zeit such bereich")
+            start = input("start angeben : ")
+            ziel = input("Ziel eingeben : ")
+            ppd = input("wass ist ein realistischer profit in % pro tag : ")
+            tagen = 1
+            startr = 0
+
+            if start == "x":
+                print("abgebrochen")
                 input("zum schließen irgendeine taste drücken")
                 exit()
+            elif start == "n":
+                print("neustart")
+                os.system(nstart)
+                exit()
+            elif ziel == "n":
+                print("neustart")
+                os.system(nstart)
+                exit()
+            elif ziel == "x":
+                print("abgebrochen")
+                input("zum schließen irgendeine taste drücken")
+                exit()
+            elif ppd == "n":
+                print("neustart")
+                os.system(nstart)
+                exit()
+            elif ppd == "x":
+                print("abgebrochen")
+                input("zum schließen irgendeine taste drücken")
+                exit()
+            else:
+                listename = f"von {str(start)} mit {str(ppd)} % bis {str(ziel)} liste.txt"
+
+                if liste == True:
+                    if os.path.exists(listename):
+                        print("genau diese datei gibt es bereits")
+                        liste = False
+                    else:
+                        file = open(str(listename), "a")
+                        file.write(f"von {str(start)} mit {str(ppd)} % bis {str(ziel)} : \n")
+                        file.write(f"tag : 0 : {str(start)} \n")
+                        file.close()
+                else:
+                    print("du hast dich gegen die liste entschieden")
+
+                if start == "0":
+                    print("der startpunkt ist zu klein!")
+                else:
+                    rechn1 = (float(ppd) / 100) + 1
+
+                    while float(start) < float(ziel):
+                        startr = round(float(start), 2)
+                        start = float(start) * float(rechn1)
+                        if liste == True:
+                            file = open(str(listename), "a")
+                            file.write(f"tag : {str(tagen)} : {str(startr)} \n")
+                            file.close()
+                        tagen += 1
+                    if float(start) > float(ziel):
+                        print("")
+
+                        if liste == True:
+                            file = open(str(listename), "a")
+                            file.write(f"tag : {str(tagen)} : {str(ziel)} \n")
+                            file.close()
+
+                    print("es braucht ", str(tagen), "tag(e) bis du ", str(ziel), "erreicht hast")
+
+                    neustart = input("n für neustart, irgendeine andere taste zum verlassen : ").lower()
+                    if neustart == "n":
+                        os.system(nstart)
+                        exit()
+                    else:
+                        input("zum schließen irgendeine taste drücken")
+                        exit()
+
+        elif auswahl == "t":
+            print("du bist im tage bereich")
+            start = input("startpunkt eingeben : ")
+            ziel = input("gib dein ziel ein : ")
+            tage = input("wie viele tage hast du zeit : ")
+
+            if start == "x":
+                print("abgebrochen")
+                input("zum schließen irgendeine taste drücken")
+                exit()
+            elif start == "n":
+                print("neustart")
+                os.system(nstart)
+                exit()
+            elif tage == "n":
+                print("neustart")
+                os.system(nstart)
+                exit()
+            elif tage == "x":
+                print("abgebrochen")
+                input("zum schließen irgendeine taste drücken")
+                exit()
+            elif ziel == "n":
+                print("neustart")
+                os.system(nstart)
+                exit()
+            elif ziel == "x":
+                print("abgebrochen")
+                input("zum schließen irgendeine taste drücken")
+                exit()
+            else:
+
+                listename = f"von {str(start)} zu {str(ziel)} in {str(tage)} tagen liste.txt"
+                tagerechn1 = float(tage) + 1
+                mindtage = 1
+
+                if float(tage) < float(mindtage):
+                    print("du hast zu wenig tage eingegeben bitte versuche es erneut")
+                    tage = input("tage wiederholen : ")
+                else:
+                    print("")
+
+                A = float(start)
+                E = float(ziel)
+                T = float(tage)
+                # rechnung, do not touch !!!
+                P = ((E / A) ** (1 / T)) - 1
+
+                proz = float(P) * 100
+                prozr = round(proz, 2)
+
+                if liste == True:
+                    if os.path.exists(listename):
+                        print("genau diese datei gibt es bereits")
+                        liste = False
+                    else:
+                        file = open(str(listename), "a")
+                        file.write(f"von {str(start)} zu {str(ziel)} in {str(tage)} benötigt : {str(prozr)} % \n")
+                        file.write(f"das Kapital würde so aussehen : \n")
+                        file.close()
+
+                    tagedone = 0
+                    startr = 0
+
+                    profitber = (float(proz) / 100) + 1
+                    while float(tagedone) < float(tagerechn1):
+                        startr = round(float(start), 2)
+                        if liste == True:
+                            file = open(str(listename), "a")
+                            file.write(f"tag {str(tagedone)} : {str(startr)} \n")
+                            file.close()
+                        start = float(start) * float(profitber)
+                        tagedone += 1
+
+                print("es braucht ca.", str(prozr), "% pro tag")
+
+                neustart = input("n für neustart, irgendeine andere taste zum verlassen : ").lower()
+                if neustart == "n":
+                    os.system(nstart)
+                    exit()
+                else:
+                    input("zum schließen irgendeine taste drücken")
+                    exit()
 
         else:
             print("fehler bitte drücke nur z oder t oder b")
@@ -217,4 +309,5 @@ else:
             else:
                 input("zum schließen irgendeine taste drücken")
                 exit()
+
 input("durchgerutscht bitte debuggen")
